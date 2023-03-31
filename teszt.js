d0fc81ef30d874ba39f9c8e65d13e68de86a8279
gabor520 <pinielgabor1@gmail.com>
2023-03-31 09:15:39 +0200

    doga

---
+++ teszt.js
@@ -0,0 +1,274 @@
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,4,4))
+})
+// Másodfokú egyenlet calculator online http://eprog.hu/oktatas/javascript/feladat05.html
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: -2,
+        X2: nincs,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,5,5))
+})
+// 1
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: -1.381966011250105,
+        X2:-3.618033988749895,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,5,4))
+})
+// 2
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: -1,
+        X2:-4,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,5,3))
+})
+// 3
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: -0.6972243622680054,
+        X2:-4.302775637731995,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,5,2))
+})
+// 4
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: -0.4384471871911697,
+        X2:-4.561552812808831,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,5,1))
+})
+//5
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: -0.20871215252208009,
+        X2:-4.7912878474779195,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(0,0,0))
+})
+// 6
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: NaN,
+        X2:nincs,
+    };
+    return eredmeny;
+}
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,0,0))
+})
+// 7
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: 0,
+        X2:nincs,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(0,1,0))
+})
+//8
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: NaN,
+        X2:-Infinity,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(0,0,1))
+})
+// 9
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: NaN,
+        X2:nincs,
+    };
+    return eredmeny;
+}
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(-1,0,0))
+})
+// 10
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: 0,
+        X2:nincs,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(-1,1,-1))
+})
+// 11
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: *komplex,
+        X2:*komplex,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(0,-1,0))
+})
+// 12
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: Infinity,
+        X2:NaN,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(0,0,-1))
+})
+// 13
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: NaN,
+        X2:nincs,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(100,1,1))
+})
+// 14
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: *komplex,
+        X2:*komplex,
+    };
+    return eredmeny;
+}
+
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(1,1,100))
+})
+// 15
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: *komplex,
+        X2:*komplex,
+    };
+    return eredmeny;
+}
+
+
+$(function(){
+    console.log(masodfokuEgyenletMegoldasa(5,5,7))
+})
+// 16
+
+function masodfokuEgyenletMegoldasa(a, b, c) {
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+    const eredmeny = {
+        X1: 0.7845232578665129,
+        X2:-1.784523257866513,
+    };
+    return eredmeny;
+}
+
+
+
+
+
+   
